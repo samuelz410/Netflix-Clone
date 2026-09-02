d276bcf3463dd27c46d4bcd0c7fffb778ca2fe6b
samuel zebene woldeyes <zebenesamuel76@gmail.com>
2026-09-03 02:52:39 +0300

    Final fix for Movies component casing

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 import Header from "./components/header";
 import Home from "./pages/home";
 import TVShows from "./pages/tv show.jsx";
-import Movies from "./pages/movie";
+import Movies from "./pages/movie.jsx";
 import Latest from "./pages/latest";
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
